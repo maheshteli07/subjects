c7fd14c5ab86f7b2f2cfedac92e8cd4624fb9e4f
maheshteli07 <maheshteli729@gmail.com>
2025-10-27 03:53:24 +0530

    first commit

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    printf("DEVOPS\n");
+    printf("JAVA\n");
+    printf("DSA\n");
+    printf("OPERATING SYSTEM\n");
+    printf("MATHEMATICS\n");
+    printf("DCCO\n");
+    printf("SOCIAL CONNECT\n");
+    printf("NCMC\n");
+    printf("CPP\n");
+
+}
